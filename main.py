1ebf20dc77833e75886ef4a9b168c70f7333fdbd
Kassym Turysbek <91659281+kassymturysbek@users.noreply.github.com>
2024-12-24 23:43:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import requests
-
-def fetch_dummy_data():
-    response = requests.get("http://127.0.0.1:5000/status")
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return {"error": "Unable to fetch data"}
-
-
+import os
+from keep_alive import keep_alive
+keep_alive()
 """
 Взаимодействие с Telegram
 """
